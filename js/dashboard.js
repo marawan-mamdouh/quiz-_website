462dad49e5ee11d1016a4f673f599c8a2a0ef4eb
Marawan Mamdouh <marawanmamdouh940@gmail.com>
2021-05-28 12:20:34 +0200

    handle all languages

---
+++ js/dashboard.js
@@ -0,0 +1,19 @@
+function html() {
+    localStorage.setItem("lang", "HTML")
+}
+
+function css() {
+    localStorage.setItem("lang", "CSS")
+}
+
+function js() {
+    localStorage.setItem("lang", "JS")
+}
+
+function python() {
+    localStorage.setItem("lang", "PY")
+}
+
+function c() {
+    localStorage.setItem("lang", "C")
+}
